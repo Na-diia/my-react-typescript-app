9b0eae49866da41c78ef48304220b142eb828628
Pekaruk Nadiia <108739131+Na-diia@users.noreply.github.com>
2023-10-28 18:10:46 +0300

    Update Events.tsx

--- src/Components/Events.tsx
+++ src/Components/Events.tsx
@@ -1,4 +1,4 @@
-import React, {FormEvent, MouseEvent, ChangeEvent, useState} from "react";
+import React, {FormEvent, MouseEvent, ChangeEvent, useState, KeyboardEvent} from "react";
 
 export function MyForm () {
     const handleSubmit = (event: FormEvent) => {
@@ -32,5 +32,52 @@ export function TextInput () {
   return (
     <input type="text" value={text} onChange={handleTextChange}/>
   )
-}
+};
+
+type InputProps = {
+    value: string | number;
+    type: 'text' | 'number';
+    onChange: (value: string | number) => void;
+    onPressEnter: () => void;
+};
+
+export function Input ({value, type, onChange, onPressEnter}: InputProps) {
+  
+    const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+        if(event.key === 'Enter') {
+            onPressEnter();
+        };
+    };
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        onChange(event.target.value);
+    };
+
+    return (
+        <input type="text"
+        onChange={handleChange} 
+        onKeyDown={handleKeyPress}/>
+    )
+};
+
+// export function Input ({value, type, onChange}: InputProps) {
+//     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+//         let newValue = event.target.value;
+
+//         if(type === 'number') {
+//           newValue = Number(newValue);
+//         }
+
+//         onChange(newValue);
+//     };
+
+//     return (
+//         <input 
+//         type="text"
+//         value={value.toString()}
+//         onChange={handleChange} />
+//     )
+// };
+
+
 
